beaa1e6b19e5b620f6992568cc6aee9215cfe550
LENOVO <LENOVO@DESKTOP-AMTT359>
2022-03-16 11:28:27 +0530

    API Assignment

---
+++ src/test/java/IBMAPIAssignment_2/IBMAPIAssignment_2/userCreationPojo.java
@@ -0,0 +1,58 @@
+package IBMAPIAssignment_2.IBMAPIAssignment_2;
+
+public class userCreationPojo {
+
+
+	private String username;
+	private String firstname;
+	private String lastname;
+	private String password;
+	private String email;
+	private String phone;
+	private String userstatus;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getUserstatus() {
+		return userstatus;
+	}
+	public void setUserstatus(String userstatus) {
+		this.userstatus = userstatus;
+	}
+	
+}
